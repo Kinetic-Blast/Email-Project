13bc59564e647e736f25ce4d0966d3a5f88ab375
Kristopher Byers <87279302+Kinetic-Blast@users.noreply.github.com>
2024-02-19 21:38:02 -0600

    Add files via upload

--- Modules/processing/AIProcessing/AIAnalysis.py
+++ Modules/processing/AIProcessing/AIAnalysis.py
@@ -1,21 +1,23 @@
 import tensorflow as tf
+import numpy as np
 import joblib
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 model = tf.keras.models.load_model('AI Training - Model/phishing_email_model.h5')
-tfidf_vectorizer = joblib.load('AI Training - Model/tfidf_vectorizer.pkl')
+tokenizer = joblib.load('AI Training - Model/tokenizer.pkl')
 
-def load_model_and_predict(input_text):
-    # Transform the input text
-    input_text_tfidf = tfidf_vectorizer.transform([input_text])
-
-    # Reorder sparse matrix indices
-    input_text_tfidf = input_text_tfidf.tocsr()  # Convert to CSR format
-    input_text_tfidf.sort_indices()
+def ai_bodytext_analysis(input_text):
+    # Tokenize input text
+    input_sequence = tokenizer.texts_to_sequences([input_text])
+    
+    # Pad sequences to a fixed length (100 tokens)
+    input_sequence_padded = pad_sequences(input_sequence, maxlen=100, padding='post')
 
     # Make predictions
-    prediction = model.predict(input_text_tfidf)
+    prediction = model.predict(input_sequence_padded)
 
     # Convert prediction probabilities to class labels
-    predicted_class = "Phishing" if prediction[0][0] > 0.5 else "Legitimate"
+    predicted_class = "Phishing" if prediction[0][0] < 0.5 else "Legitimate"
 
     return predicted_class
